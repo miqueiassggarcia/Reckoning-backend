a213402a1a8a1dbd74b7eeef0382287274ecc724
Noberto Nunes <81520797+NobertoNNJ@users.noreply.github.com>
2023-05-27 19:15:49 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client"
 
+import { VersaoSchema } from "./validation/Versao";
+import { FeedbackSchema } from "./validation/Feedback";
+import { ImagemSchema } from "./validation/Imagem";
+
 const app = express();
 app.use(express.json());
 
@@ -8,8 +12,65 @@ const prisma = new PrismaClient({
   log:["query"]
 });
 
-app.get("/", (request, response) => {
-  return response.json({"teste": "Deu certo!"})
-})
+// Rota para cadastro de uma nova versão
+app.post("/Versao", async (request, response) => {
+  const {
+    idVersao,
+    nome,
+    descricao,
+    data,
+    arquivo
+  } = VersaoSchema.parse(request.body);
 
-app.listen(3333);
+  const versao = await prisma.versao.create({
+    data: {
+      idVersao: idVersao,
+      nome: nome,
+      descricao: descricao,
+      data: data,
+      arquivo: arquivo
+    }
+  });
+
+  return response.status(201).json(versao);
+});
+
+// Rota para cadastro de um feedback
+app.post("/Feedback", async (request, response) => {
+  const {
+    idFeedback,
+    atribuicao,
+    feedback,
+  } = FeedbackSchema.parse(request.body);
+
+  const feedbacks = await prisma.feedback.create({
+    data: {
+      idFeedback: idFeedback,
+      atribuicao: atribuicao,
+      feedback: feedback
+    }
+  });
+
+  return response.status(201).json(feedbacks);
+});
+
+// Rota para inserção de Imagem
+app.post("/Imagem", async (request, response) => {
+  const {
+    idImagem,
+    personagemIdPersonagem,
+    itemIdItem,
+    Imagem
+  } = ImagemSchema.parse(request.body);
+
+  const imagens = await prisma.imagem.create({
+    data: {
+      idImagem: idImagem,
+      personagemIdPersonagem: personagemIdPersonagem,
+      itemIdItem: itemIdItem,
+      imagem: Imagem
+    }
+  });
+
+  return response.status(201).json(imagens);
+});
