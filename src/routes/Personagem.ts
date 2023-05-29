3d684e9d7c981cd9bad47726715e1c9f6747da23
Noberto Nunes <81520797+NobertoNNJ@users.noreply.github.com>
2023-05-28 22:02:28 -0300

    Update Personagem.ts

--- src/routes/Personagem.ts
+++ src/routes/Personagem.ts
@@ -5,88 +5,117 @@ import { PersonagemSchema } from "../validation/Personagem";
 module.exports = (app: Express, prisma: PrismaClient) => {
   // Rota para inserção de personagens
   app.post("/personagem", async (request, response) => {
-    const {
-      imagemIdImagem,
-      nome,
-      descricao
-    } = PersonagemSchema.parse(request.body);
+    try{
+      const {
+        imagemIdImagem,
+        nome,
+        descricao
+      } = PersonagemSchema.parse(request.body);
 
-    const personagem = await prisma.personagem.create({
-      data: {
-        imagemIdImagem: imagemIdImagem,
-        nome: nome,
-        descricao: descricao
-      }
-    })
-
-    return response.status(201).json(personagem);
-  })
+      const personagem = await prisma.personagem.create({
+        data: {
+          imagemIdImagem: imagemIdImagem,
+          nome: nome,
+          descricao: descricao
+        }
+      });
+      return response.status(201).json(personagem);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(400).json({error: 'occoreu um erro ao cadastrar o personagem'});
+    }
+});
 
   // Rota para pegar personagem
   app.get("/personagem/:id", async (request, response) => {
-    const idPersonagens = request.params.id;
+    try{
+      const idPersonagens = request.params.id;
 
-    const personagem = await prisma.personagem.findUniqueOrThrow({
-      select: {
-        imagemIdImagem: true,
-        nome: true,
-        descricao: true
-      },
-      where: {
-        idPersonagem: idPersonagens
+      const personagem = await prisma.personagem.findUniqueOrThrow({
+        select: {
+          imagemIdImagem: true,
+          nome: true,
+          descricao: true
+        },
+        where: {
+          idPersonagem: idPersonagens
+        }
+      });
+      if (!personagem) {
+        return response.status(404).json({ error: 'Personagem não encontrado.' });
       }
-    })
-
-    return response.json(personagem);
-  })
+      return response.json(personagem);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao buscar personagem'});
+    }
+  });
 
   // Rota para pegar todos os personagens
   app.get("/personagem/", async (request, response) => {
-    const personagens = await prisma.personagem.findMany({
-      select: {
-        imagemIdImagem: true,
-        nome: true,
-        descricao: true
-      }
-    });
-
-    return response.json(personagens);
+    try{
+      const personagens = await prisma.personagem.findMany({
+        select: {
+          imagemIdImagem: true,
+          nome: true,
+          descricao: true
+        }
+      });
+      return response.json(personagens);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao buscar personagens'});
+    }
   });
   
   // Rota para deletar personagem
   app.delete("/personagem/:id", async (request, response) => {
-    const idPersonagem = request.params.id;
+    try{
+      const idPersonagem = request.params.id;
 
-    const personagem = await prisma.personagem.delete({
-      where: {
-        idPersonagem: idPersonagem
+      const personagem = await prisma.personagem.delete({
+        where: {
+          idPersonagem: idPersonagem
+        }
+      });
+      if (!personagem) {
+        return response.status(404).json({ error: 'Personagem não encontrado.' });
       }
-    })
-
-    return response.json(personagem);
+      return response.json(personagem);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao deletar personagem'});
+    }
   });
 
   // Rota para atualizar imagem
   app.put("/personagem/:id", async (request, response) => {
-    const { descricao, imagemIdImagem, nome } = PersonagemSchema.partial().parse(request.body);
+    try{
+      const { descricao, imagemIdImagem, nome } = PersonagemSchema.partial().parse(request.body);
 
-    if(descricao || imagemIdImagem || nome) {
-      const idPersonagem = request.params.id;
+      if(descricao || imagemIdImagem || nome) {
+        const idPersonagem = request.params.id;
 
-      const novoPersonagem = await prisma.personagem.update({
-        where: {
-          idPersonagem: idPersonagem
-        },
-        data: {
-          imagemIdImagem: imagemIdImagem,
-          descricao: descricao,
-          nome: nome
+        const novoPersonagem = await prisma.personagem.update({
+          where: {
+            idPersonagem: idPersonagem
+          },
+          data: {
+            imagemIdImagem: imagemIdImagem,
+            descricao: descricao,
+            nome: nome
+          }
+        });
+        if (!novoPersonagem) {
+          return response.status(404).json({ error: 'Personagem não encontrado.' });
         }
-      })
-
-      return response.json(novoPersonagem);
-    } else {
-      return response.json({"message": "Nada foi modificado"})
+        return response.json(novoPersonagem);
+      } else {
+        return response.status(400).json({"message": "Nada foi modificado"})
+      }
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao atualizar personagem'});
     }
   });
 }
