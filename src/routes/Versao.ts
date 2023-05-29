2aefc1526f3c65e3db5774f68f0aed2f2f6ec6dc
Noberto Nunes <81520797+NobertoNNJ@users.noreply.github.com>
2023-05-28 21:28:17 -0300

    Update Versao.ts

--- src/routes/Versao.ts
+++ src/routes/Versao.ts
@@ -5,71 +5,95 @@ import { VersaoSchema } from "../validation/Versao";
 module.exports = (app: Express, prisma: PrismaClient) => {
   // Rota para cadastro de uma nova versão
   app.post("/versao", async (request, response) => {
-    const {
-      nome,
-      descricao,
-      arquivo
-    } = VersaoSchema.parse(request.body);
+    try{
+      const {
+        nome,
+        descricao,
+        arquivo
+      } = VersaoSchema.parse(request.body);
 
-    const versao = await prisma.versao.create({
-      data: {
-        nome: nome,
-        descricao: descricao,
-        arquivo: arquivo
-      }
-    });
+      const versao = await prisma.versao.create({
+        data: {
+          nome: nome,
+          descricao: descricao,
+          arquivo: arquivo
+        }
+      });
 
-    return response.status(201).json(versao);
-  });
+      return response.status(201).json(versao);
+    }catch(error){
+    console.error('ocorreu um erro:', error);
+    return response.status(400).json({error: 'occoreu um erro ao cadastrar a versão'});
+  }
+});
 
   // Rota para pegar versao
   app.get("/versao/:id", async (request, response) => {
-    const idVersao = request.params.id;
+    try{
+      const idVersao = request.params.id;
 
-    const versao = await prisma.versao.findUniqueOrThrow({
-      select: {
-        nome: true,
-        descricao: true,
-        data: true,
-        arquivo: true
-      },
-      where: {
-        idVersao: idVersao
+      const versao = await prisma.versao.findUniqueOrThrow({
+        select: {
+          nome: true,
+          descricao: true,
+          data: true,
+          arquivo: true
+        },
+        where: {
+          idVersao: idVersao
+        }
+      });
+      if (!versao) {
+        return response.status(404).json({ error: 'Versão não encontrado.' });
       }
-    });
-
-    return response.json(versao);
-  })
+      return response.json(versao);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao buscar a versão'});
+    }
+});
   
   // Rota para pegar todas as versões(acho que nem precisa)
   app.get("/versao/", async (request, response) => {
-    const versao = await prisma.versao.findMany({
-      select: {
-        nome: true,
-        descricao:true,
-        data: true,
-        arquivo: true
-      }
-    });
-
-    return response.json(versao);
-  });
+    try{
+      const versao = await prisma.versao.findMany({
+        select: {
+          nome: true,
+          descricao:true,
+          data: true,
+          arquivo: true
+        }
+      });
+      return response.json(versao);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao buscar as versões'});
+    }
+});
 
   // Rota para deletar versao
   app.delete("/versao/:id", async (request, response) => {
-    const idVersao = request.params.id;
+    try{
+      const idVersao = request.params.id;
 
-    const versao = await prisma.versao.delete({
-      where: {
-        idVersao: idVersao
+      const versao = await prisma.versao.delete({
+        where: {
+          idVersao: idVersao
+        }
+      });
+      if (!versao) {
+        return response.status(404).json({ error: 'Versão não encontrado.' });
       }
-    })
-
-    return response.json(versao);
-  });
+      return response.json(versao);
+    }catch(error){
+      console.error('ocorreu um erro:', error);
+      return response.status(500).json({error: 'occoreu um erro ao deletar a versão'});
+    }
+});
 
   // Rota para atualizar versao
   app.put("/versao/:id", async (request, response) => {
+    try{
     const {nome, arquivo, descricao} = VersaoSchema.partial().parse(request.body);
 
     if(nome || arquivo || descricao) {
@@ -84,11 +108,17 @@ module.exports = (app: Express, prisma: PrismaClient) => {
           arquivo: arquivo,
           descricao: descricao
         }
-      })
-
+      });
+      if (!versao) {
+        return response.status(404).json({ error: 'Versão não encontrada.' });
+      }
       return response.json(versao);
     } else {
-      return response.json({"message": "Nada foi modificado"})
+      return response.status(400).json({"message": "Nada foi modificado"})
     }
+  }catch(error){
+    console.error('ocorreu um erro:', error);
+    return response.status(500).json({error: 'occoreu um erro ao deletar a versão'});
+  }
   });
 }
