dc177438854b72ddad03df20ac64bd236b9dc305
Noberto Nunes <81520797+NobertoNNJ@users.noreply.github.com>
2023-05-30 14:32:54 -0300

    Update versao.test.ts

--- src/test/versao.test.ts
+++ src/test/versao.test.ts
@@ -9,42 +9,69 @@ let data = currentDateTime.toISOString();
 let arquivo = "https://reckoning-version1.1.2"
 
 describe("Testar rota post de versao", () => {
-  it("Deve criar uma versao", async () => {
-    const res = await request(app).post("/versao").send({
-      "idVersao": `${idVersao}`,
-      "nome": `${nome}`,
-      "descricao": `${descricao}`,
-      "data": `${data}`,
-      "arquivo": `${arquivo}`
+    it("Deve criar uma versao", async () => {
+        const res = await request(app).post("/versao").send({
+            "idVersao": `${idVersao}`,
+            "nome": `${nome}`,
+            "descricao": `${descricao}`,
+            "data": `${data}`,
+            "arquivo": `${arquivo}`
+        });
+        expect(res.statusCode).toBe(201);
+        expect(res.body.idVersao).toBe(`${idVersao}`);
+        expect(res.body.nome).toBe(`${nome}`);
+        expect(res.body.descricao).toBe(`${descricao}`);
+        expect(res.body.data).toBe(`${data}`);
+        expect(res.body.arquivo).toBe(`${arquivo}`);
     });
-    expect(res.statusCode).toBe(201);
-    expect(res.body.idVersao).toBe(`${idVersao}`);
-    expect(res.body.nome).toBe(`${nome}`);
-    expect(res.body.descricao).toBe(`${descricao}`);
-    expect(res.body.data).toBe(`${data}`);
-    expect(res.body.arquivo).toBe(`${arquivo}`);
-  });
+    it('Deve retornar status 400 se nada for cadastrado', async () => {
+        const res = await request(app)
+            .post('/versao')
+            .send({});
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('message', 'Nada foi cadastrado');
+    });
 });
 
 
 describe("Testar rota get de uma versao", () => {
-  it("Deve responder uma versao", async () => {
-    const res = await request(app).get(`/versao/${idVersao}`);
-    expect(res.statusCode).toBe(200);
-    expect(res.body.nome).toBe(`${nome}`);
-    expect(res.body.descricao).toBe(`${descricao}`);
-    expect(res.body.data).toBe(`${data}`);
-    expect(res.body.arquivo).toBe(`${arquivo}`);
-  });
+    it("Deve responder uma versao", async () => {
+        const res = await request(app).get(`/versao/${idVersao}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.nome).toBe(`${nome}`);
+        expect(res.body.descricao).toBe(`${descricao}`);
+        expect(res.body.data).toBe(`${data}`);
+        expect(res.body.arquivo).toBe(`${arquivo}`);
+    });
+    it('Deve retornar status 404 para uma versão inexistente', async () => {
+        let idVersaoo = "341c-8cb4-4fb9";
+        const res = await request(app).get(`/versao/${idVersaoo}`); 
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty('error', 'Versão não encontrada.');
+        });
+    it('Deve retornar status 500 em caso de erro interno', async () => {
+        // Simulando um erro interno no servidor
+        jest.spyOn(prisma.versao, 'findUniqueOrThrow').mockRejectedValueOnce(new Error('Erro interno'));
+        const res = await request(app).get(`/versao/${idVersao}`);
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('error', 'occoreu um erro ao procurar versão');
+    });
 });
 
 
 describe("Testar rota get de listar as versoes", () => {
-  it("Deve listar todas as versoes", async () => {
-    const res = await request(app).get("/versao");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.length).toBeGreaterThan(0);
-  });
+    it("Deve listar todas as versoes", async () => {
+        const res = await request(app).get("/versao");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+    });
+    it('Deve retornar status 500 em caso de erro interno', async () => {
+        // Simulando um erro interno no servidor
+        jest.spyOn(prisma.versao, 'findMany').mockRejectedValueOnce(new Error('Erro interno'));
+        const res = await request(app).get(`/versao`);
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('error', 'occoreu um erro ao procurar versões');
+    });
 });
 
 
@@ -55,32 +82,63 @@ arquivo = "https://reckoning-version1.1.2"
 
 
 describe("Testar rota de atualizar a versao", () => {
-  it("Deve atualizar a versao", async () => {
-    const res = await request(app).put(`/versao/${idVersao}`).send({
-        "nome": `${nome}`,
-        "descricao": `${descricao}`,
-        "data": `${data}`,
-        "arquivo": `${arquivo}`
+    it("Deve atualizar a versao", async () => {
+        const res = await request(app).put(`/versao/${idVersao}`).send({
+            "nome": `${nome}`,
+            "descricao": `${descricao}`,
+            "data": `${data}`,
+            "arquivo": `${arquivo}`
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.nome).toBe(`${nome}`);
+        expect(res.body.descricao).toBe(`${descricao}`);
+        expect(res.body.data).toBe(`${data}`);
+        expect(res.body.arquivo).toBe(`${arquivo}`);
     });
-
-
-    expect(res.statusCode).toBe(200);
-    expect(res.body.nome).toBe(`${nome}`);
-    expect(res.body.descricao).toBe(`${descricao}`);
-    expect(res.body.data).toBe(`${data}`);
-    expect(res.body.arquivo).toBe(`${arquivo}`);
-  });
+    it('Deve retornar status 404 para uma versão inexistente', async () => {
+        let idVersaoo = "341c-8cb4-4fb9";
+        const res = await request(app).put(`/versao/${idVersaoo}`); 
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty('error', 'Versão não encontrada.');
+    });
+    it('Deve retornar status 400 se nada for modificado', async () => {
+        const res = await request(app)
+        .put(`/versao/${idVersao}`)
+        .send({});
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('message', 'Nada foi modificado');
+    });
+    it('Deve retornar status 500 em caso de erro interno', async () => {
+        // Simulando um erro interno no servidor
+        jest.spyOn(prisma.versao, 'update').mockRejectedValueOnce(new Error('Erro interno'));
+        const res = await request(app).put(`/versao/${idVersao}`);
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('error', 'occoreu um erro ao modificar versão');
+    });
 });
 
 
 describe("Testar rota de deletar feedback", () => {
-  it("Deve deletar o feedback solicitado", async () => {
-    const res = await request(app).delete(`/versao/${idVersao}`);
-    expect(res.statusCode).toBe(200);
-    expect(res.body.idVersao).toBe(`${idVersao}`);
-    expect(res.body.nome).toBe(`${nome}`);
-    expect(res.body.descricao).toBe(`${descricao}`);
-    expect(res.body.data).toBe(`${data}`);
-    expect(res.body.arquivo).toBe(`${arquivo}`);
-  });
+    it("Deve deletar o feedback solicitado", async () => {
+        const res = await request(app).delete(`/versao/${idVersao}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.idVersao).toBe(`${idVersao}`);
+        expect(res.body.nome).toBe(`${nome}`);
+        expect(res.body.descricao).toBe(`${descricao}`);
+        expect(res.body.data).toBe(`${data}`);
+        expect(res.body.arquivo).toBe(`${arquivo}`);
+    });
+    it('Deve retornar status 404 para uma versão inexistente', async () => {
+        let idVersaoo = "341c-8cb4-4fb9";
+        const res = await request(app).delete(`/versao/${idVersaoo}`); 
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty('error', 'Versão não encontrada.');
+    });
+    it('Deve retornar status 500 em caso de erro interno', async () => {
+        // Simulando um erro interno no servidor
+        jest.spyOn(prisma.versao, 'delete').mockRejectedValueOnce(new Error('Erro interno'));
+        const res = await request(app).delete(`/versao/${idVersao}`);
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('error', 'occoreu um erro ao deletar versão');
+    });
 });
